dd889739eb20a82731a6a6dff6c85ee0f0c1112f
YX-SGX <cyx.xjtu@gmail.com>
2019-04-09 16:39:24 +0800

    Implement e_rsa_ecdsa_init() for creating public key pair and signature key pair

--- app-src/main.c
+++ app-src/main.c
@@ -61,14 +61,30 @@ int main(int argc, char** argv)
   }
   fclose(fp);
 
-  // char param_a[1024]="type a\nq 8780710799663312522437781984754049815806883199414208211028653399266475630880222957078625179422662221423155858769582317459277713367317481324925129998224791\nh 12016012264891146079388821366740534204802954401251311822919615131047207289359704531102844802183906537786776\nr 730750818665451621361119245571504901405976559617\nexp2 159\nexp1 107\nsign1 1\nsign0 1\n";
-
   sgx_status_t ret = SGX_SUCCESS;
   e_pairing_init(global_eid, &ret, param, count);
   if(SGX_SUCCESS != ret){
     printf("Enclave initialize pairing error!\n");
   }else{
     printf("Enclave initialize pairing ok!\n");
+  }
+
+  e_rsa_ecdsa_init(global_eid, &ret, 32, 4);
+  if(SGX_SUCCESS != ret){
+    printf("Enclave initialize rsa and ecdsa (signature) error!\n");
+    switch(ret){
+        case SGX_ERROR_INVALID_PARAMETER:
+          printf("Invalid parameter!\n");
+          break;
+        case SGX_ERROR_OUT_OF_MEMORY:
+          printf("Out of memory!\n");
+          break;
+        case SGX_ERROR_UNEXPECTED:
+          printf("Error unexpected\n");
+          break;
+    }
+  }else{
+    printf("Enclave initialize rsa and ecdsa (signature) ok!\n");
   }
 
   /* Destroy the enclave */
--- enc-src/enclave.c
+++ enc-src/enclave.c
@@ -16,6 +16,11 @@
 pairing_t g_pairing; //for the used bilinear map
 errmsg_s_ptr g_errmsg;
 
+void *g_rsa_pri_key;
+void *g_rsa_pub_key;
+sgx_ec256_private_t *g_ecdsa_sign_key;
+sgx_ec256_public_t *g_ecdsa_verify_key;
+
 void eprintf(const char *fmt, ...){
   char buf[BUFSIZ] = {'\0'};
   va_list ap;
@@ -52,8 +57,57 @@ sgx_status_t e_pairing_init(char* param, size_t count){
 
 }
 
-sgx_status_t e_rsa_ecdsa_init(){
+sgx_status_t e_rsa_ecdsa_init(int n_byte_size, int e_byte_size){
   sgx_status_t ret = SGX_SUCCESS;
+
+  unsigned char *p_e = (unsigned char *)malloc(e_byte_size);   // unsigned char p_e[4] = {1, 0, 1, 0};
+  *p_e = '1';
+  *(p_e+1) = '0';
+  *(p_e+2) = '1';
+  *(p_e+3) = '0';
+  unsigned char *p_n = (unsigned char *)malloc(n_byte_size);
+  unsigned char *p_d = (unsigned char *)malloc(n_byte_size);
+  unsigned char *p_p = (unsigned char *)malloc(n_byte_size);
+  unsigned char *p_q = (unsigned char *)malloc(n_byte_size);
+  unsigned char *p_dmp1 = (unsigned char *)malloc(n_byte_size);
+  unsigned char *p_dmq1 = (unsigned char *)malloc(n_byte_size);
+  unsigned char *p_iqmp = (unsigned char *)malloc(n_byte_size);
+
+  sgx_ecc_state_handle_t ecc_handle;
+  g_ecdsa_sign_key= (sgx_ec256_private_t *)malloc(32);
+  g_ecdsa_verify_key = (sgx_ec256_public_t *)malloc(32);
+
+  ret = sgx_create_rsa_key_pair(n_byte_size, e_byte_size, p_n, p_d, p_e, p_p, p_q, p_dmp1, p_dmq1, p_iqmp);
+  if(SGX_SUCCESS != ret){
+    eprintf("[Err]: creating rsa key pair components failed\n");
+    return ret;
+  }
+  ret = sgx_create_rsa_priv2_key(n_byte_size, e_byte_size, p_e, p_p, p_q, p_dmp1, p_dmq1, p_iqmp, &g_rsa_pri_key);
+  if(SGX_SUCCESS != ret){
+    eprintf("[Err]: creating rsa private key failed\n");
+    return ret;
+  }
+  ret = sgx_create_rsa_pub1_key(n_byte_size, e_byte_size, p_n, p_e, &g_rsa_pub_key);
+  if(SGX_SUCCESS != ret){
+    eprintf("[Err]: creating rsa public key failed\n");
+    return ret;
+  }
+
+  ret = sgx_ecc256_open_context(&ecc_handle);
+  if(SGX_SUCCESS != ret){
+    eprintf("[Err]: ecc256 context open failed\n");
+    return ret;
+  }
+  ret = sgx_ecc256_create_key_pair(g_ecdsa_sign_key, g_ecdsa_verify_key, ecc_handle);
+  if(SGX_SUCCESS != ret){
+    eprintf("[Err]: creating signature key pair failed\n");
+    return ret;
+  }
+  ret = sgx_ecc256_close_context(ecc_handle);
+  if(SGX_SUCCESS != ret){
+    eprintf("[Err]: ecc256 context close failed\n");
+    return ret;
+  }
 
   return ret;
 }
