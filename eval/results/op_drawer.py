5ae8c831af308b8ad883ea869247f2d1b2466e1f
YX-SGX <cyx.xjtu@gmail.com>
2019-12-24 17:58:28 +0800

    evaluation result update

--- eval/results/op_drawer.py
+++ eval/results/op_drawer.py
@@ -2,13 +2,18 @@ import pylab
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 import numpy as np
+import math
 
 def ms_data(x):
 	y = float(x) * 1000
 	return y
 
 def throughput_data(x):
-	y = int(1/float(x))
+	y = 1/float(x)
+	return y
+
+def log_data(x):
+	y = math.log(x)
 	return y
 
 def load_data(file_name):
@@ -27,34 +32,62 @@ def load_data(file_name):
 (nai_p_file_num, nai_p_exe_time) = load_data('./nai_projector.txt')
 nai_p_exe_time_ms = list(map(ms_data, nai_p_exe_time))
 nai_p_throught_put = list(map(throughput_data, nai_p_exe_time))
+nai_p_throught_put_log = list(map(log_data, nai_p_throught_put))
 
 (nai_s_file_num, nai_s_exe_time) = load_data('./nai_selector.txt')
 nai_s_exe_time_ms = list(map(ms_data, nai_s_exe_time))
 nai_s_throught_put = list(map(throughput_data, nai_s_exe_time))
+nai_s_throught_put_log = list(map(log_data, nai_s_throught_put))
 
 (nai_j_file_num, nai_j_exe_time) = load_data('./nai_joiner.txt')
 nai_j_exe_time_ms = list(map(ms_data, nai_j_exe_time))
 nai_j_throught_put = list(map(throughput_data, nai_j_exe_time))
+nai_j_throught_put_log = list(map(log_data, nai_j_throught_put))
 
 (nai_a_file_num, nai_a_exe_time) = load_data('./nai_aggregator.txt')
 nai_a_exe_time_ms = list(map(ms_data, nai_a_exe_time))
 nai_a_throught_put = list(map(throughput_data, nai_a_exe_time))
+nai_a_throught_put_log = list(map(log_data, nai_a_throught_put))
 
 (sgx_p_file_num, sgx_p_exe_time) = load_data('./sgx_projector.txt')
 sgx_p_exe_time_ms = list(map(ms_data, sgx_p_exe_time))
 sgx_p_throught_put = list(map(throughput_data, sgx_p_exe_time))
+sgx_p_throught_put_log = list(map(log_data, sgx_p_throught_put))
 
 (sgx_s_file_num, sgx_s_exe_time) = load_data('./sgx_selector.txt')
 sgx_s_exe_time_ms = list(map(ms_data, sgx_s_exe_time))
 sgx_s_throught_put = list(map(throughput_data, sgx_s_exe_time))
+sgx_s_throught_put_log = list(map(log_data, sgx_s_throught_put))
 
 (sgx_j_file_num, sgx_j_exe_time) = load_data('./sgx_joiner.txt')
 sgx_j_exe_time_ms = list(map(ms_data, sgx_j_exe_time))
 sgx_j_throught_put = list(map(throughput_data, sgx_j_exe_time))
+sgx_j_throught_put_log = list(map(log_data, sgx_j_throught_put))
 
 (sgx_a_file_num, sgx_a_exe_time) = load_data('./sgx_aggregator.txt')
 sgx_a_exe_time_ms = list(map(ms_data, sgx_a_exe_time))
 sgx_a_throught_put = list(map(throughput_data, sgx_a_exe_time))
+sgx_a_throught_put_log = list(map(log_data, sgx_a_throught_put))
+
+(cdb_p_file_num, cdb_p_exe_time) = load_data('./cdb_projector.txt')
+cdb_p_exe_time_ms = list(map(ms_data, cdb_p_exe_time))
+cdb_p_throught_put = list(map(throughput_data, cdb_p_exe_time))
+cdb_p_throught_put_log = list(map(log_data, cdb_p_throught_put))
+
+(cdb_s_file_num, cdb_s_exe_time) = load_data('./cdb_selector.txt')
+cdb_s_exe_time_ms = list(map(ms_data, cdb_s_exe_time))
+cdb_s_throught_put = list(map(throughput_data, cdb_s_exe_time))
+cdb_s_throught_put_log = list(map(log_data, cdb_s_throught_put))
+
+(cdb_j_file_num, cdb_j_exe_time) = load_data('./cdb_joiner.txt')
+cdb_j_exe_time_ms = list(map(ms_data, cdb_j_exe_time))
+cdb_j_throught_put = list(map(throughput_data, cdb_j_exe_time))
+cdb_j_throught_put_log = list(map(log_data, cdb_j_throught_put))
+
+(cdb_a_file_num, cdb_a_exe_time) = load_data('./cdb_aggregator.txt')
+cdb_a_exe_time_ms = list(map(ms_data, cdb_a_exe_time))
+cdb_a_throught_put = list(map(throughput_data, cdb_a_exe_time))
+cdb_a_throught_put_log = list(map(log_data, cdb_a_throught_put))
 
 (e_size_l, e_exe_time_l) = load_data('./escheme_sgx_dec_1.txt')
 e_exe_time_l_ms = list(map(ms_data, e_exe_time_l))
@@ -73,7 +106,7 @@ sgx_exe_time_h_ms = list(map(ms_data, sgx_exe_time_h))
 #fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2,2)
 
 fig = plt.figure()
-gs = gridspec.GridSpec(nrows=3, ncols=2, height_ratios=[1,1,1.2])
+gs = gridspec.GridSpec(nrows=3, ncols=2, height_ratios=[1,1,1])
 ax1 = fig.add_subplot(gs[0,0])
 ax2 = fig.add_subplot(gs[0,1])
 ax3 = fig.add_subplot(gs[1,0])
@@ -81,96 +114,107 @@ ax4 = fig.add_subplot(gs[1,1])
 ax5 = fig.add_subplot(gs[2, :])
 
 bar_width=0.2
-op_x_data = ['10', '10K', '50K']
+op_x_data = ['1K', '10K', '50K']
 op_j_x_data = ['50', '150', '250']
 
 nai_p_y_data = []
-nai_p_y_data.append(nai_p_throught_put[0])
-nai_p_y_data.append(nai_p_throught_put[3])
-nai_p_y_data.append(nai_p_throught_put[7])
+nai_p_y_data.append(nai_p_throught_put_log[2])
+nai_p_y_data.append(nai_p_throught_put_log[3])
+nai_p_y_data.append(nai_p_throught_put_log[7])
 sgx_p_y_data = []
-sgx_p_y_data.append(sgx_p_throught_put[0])
-sgx_p_y_data.append(sgx_p_throught_put[3])
-sgx_p_y_data.append(sgx_p_throught_put[7])
+sgx_p_y_data.append(sgx_p_throught_put_log[2])
+sgx_p_y_data.append(sgx_p_throught_put_log[3])
+sgx_p_y_data.append(sgx_p_throught_put_log[7])
 # sgx_pure_p_y_data = [900, 870, 700]
-cdb_p_y_data = [120,100,80]
+cdb_p_y_data = []
+cdb_p_y_data.append(cdb_p_throught_put_log[0])
+cdb_p_y_data.append(cdb_p_throught_put_log[1])
+cdb_p_y_data.append(cdb_p_throught_put_log[2])
+
 ax1.set(title='(a). projection')
 ax1.grid(linestyle='--', zorder=1)
 ax1.barh(y=range(len(op_x_data)), width=nai_p_y_data, label='Baseline', zorder=2, color='slategrey', alpha=1, height=bar_width)
-# ax1.barh(y=np.arange(len(op_x_data))+bar_width, width=sgx_pure_p_y_data, label='QShield', zorder=2, color='forestgreen', alpha=1, height=bar_width)
 ax1.barh(y=np.arange(len(op_x_data))+ 1*bar_width, width=sgx_p_y_data, label='QShield', zorder=2, color='chocolate', alpha=1, height=bar_width)
 ax1.barh(y=np.arange(len(op_x_data))+ 2*bar_width, width=cdb_p_y_data, label='CryptDB', zorder=2, color='forestgreen', alpha=1, height=bar_width)
 ax1.set_yticks(np.arange(len(op_x_data))+bar_width)
 ax1.set_yticklabels(op_x_data)
-ax1.set(xlim=[0,1500])
-ax1.legend(loc='upper right', frameon=False, fontsize=6)
-ax1.set(xlabel="Throughput (projectors/sec)", ylabel="Doc Numbers")
+ax1.set(xlim=[0,10])
+ax1.legend(loc='upper right', frameon=False, fontsize=5)
+ax1.set(xlabel="Log of Throughput", ylabel="Doc Numbers")
 
 nai_s_y_data = []
-nai_s_y_data.append(nai_s_throught_put[0])
-nai_s_y_data.append(nai_s_throught_put[3])
-nai_s_y_data.append(nai_s_throught_put[7])
+nai_s_y_data.append(nai_s_throught_put_log[2])
+nai_s_y_data.append(nai_s_throught_put_log[3])
+nai_s_y_data.append(nai_s_throught_put_log[7])
 sgx_s_y_data = []
-sgx_s_y_data.append(sgx_s_throught_put[0])
-sgx_s_y_data.append(sgx_s_throught_put[3])
-sgx_s_y_data.append(sgx_s_throught_put[7])
+sgx_s_y_data.append(sgx_s_throught_put_log[2])
+sgx_s_y_data.append(sgx_s_throught_put_log[3])
+sgx_s_y_data.append(sgx_s_throught_put_log[7])
 # sgx_pure_s_y_data = [900, 870, 700]
-cdb_s_y_data = [120,100,80]
+cdb_s_y_data = []
+cdb_s_y_data.append(cdb_s_throught_put_log[0])
+cdb_s_y_data.append(cdb_s_throught_put_log[1])
+cdb_s_y_data.append(cdb_s_throught_put_log[2])
+
 ax2.set(title='(b). selection')
 ax2.grid(linestyle='--', zorder=1)
 ax2.barh(y=range(len(op_x_data)), width=nai_s_y_data, label='Baseline', zorder=2, color='slategrey', alpha=1, height=bar_width)
-# ax2.barh(y=np.arange(len(op_x_data))+bar_width, width=sgx_pure_s_y_data, label='QShield', zorder=2, color='forestgreen', alpha=1, height=bar_width)
-ax2.barh(y=np.arange(len(op_x_data))+1*bar_width, width=sgx_s_y_data, label='QShield', zorder=2, color='chocolate', alpha=1, height=bar_width)
+ax2.barh(y=np.arange(len(op_x_data))+ 1*bar_width, width=sgx_s_y_data, label='QShield', zorder=2, color='chocolate', alpha=1, height=bar_width)
 ax2.barh(y=np.arange(len(op_x_data))+ 2*bar_width, width=cdb_s_y_data, label='CryptDB', zorder=2, color='forestgreen', alpha=1, height=bar_width)
 ax2.set_yticks(np.arange(len(op_x_data))+bar_width)
 ax2.set_yticklabels(op_x_data)
-ax2.set(xlim=[0,1500])
-ax2.legend(loc='upper right', frameon=False, fontsize=6)
-ax2.set(xlabel="Throughput (selectors/sec)", ylabel="Doc Numbers")
+ax2.set(xlim=[0,10])
+ax2.legend(loc='upper right', frameon=False, fontsize=5)
+ax2.set(xlabel="Log of Throughput", ylabel="Doc Numbers")
 
 nai_a_y_data = []
-nai_a_y_data.append(nai_a_throught_put[0])
-nai_a_y_data.append(nai_a_throught_put[3])
-nai_a_y_data.append(nai_a_throught_put[7])
+nai_a_y_data.append(nai_a_throught_put_log[2])
+nai_a_y_data.append(nai_a_throught_put_log[3])
+nai_a_y_data.append(nai_a_throught_put_log[7])
 sgx_a_y_data = []
-sgx_a_y_data.append(sgx_a_throught_put[0])
-sgx_a_y_data.append(sgx_a_throught_put[3])
-sgx_a_y_data.append(sgx_a_throught_put[7])
+sgx_a_y_data.append(sgx_a_throught_put_log[2])
+sgx_a_y_data.append(sgx_a_throught_put_log[3])
+sgx_a_y_data.append(sgx_a_throught_put_log[7])
 # sgx_pure_a_y_data = [900, 870, 700]
-cdb_a_y_data = [120,100,80]
+cdb_a_y_data = []
+cdb_a_y_data.append(cdb_a_throught_put_log[0])
+cdb_a_y_data.append(cdb_a_throught_put_log[1])
+cdb_a_y_data.append(cdb_a_throught_put_log[2])
+
 ax3.set(title='(c). aggregation')
 ax3.grid(linestyle='--', zorder=1)
 ax3.barh(y=range(len(op_x_data)), width=nai_a_y_data, label='Baseline', zorder=2, color='slategrey', alpha=1, height=bar_width)
-# ax3.barh(y=np.arange(len(op_x_data))+bar_width, width=sgx_pure_a_y_data, label='QShield', zorder=2, color='forestgreen', alpha=1, height=bar_width)
 ax3.barh(y=np.arange(len(op_x_data))+ 1*bar_width, width=sgx_a_y_data, label='QShield', zorder=2, color='chocolate', alpha=1, height=bar_width)
 ax3.barh(y=np.arange(len(op_x_data))+ 2*bar_width, width=cdb_a_y_data, label='CryptDB', zorder=2, color='forestgreen', alpha=1, height=bar_width)
 ax3.set_yticks(np.arange(len(op_x_data))+bar_width)
 ax3.set_yticklabels(op_x_data)
-ax3.set(xlim=[0,1500])
-ax3.legend(loc='upper right', frameon=False, fontsize=6)
-ax3.set(xlabel="Throughput (aggregators/sec)", ylabel="Doc Numbers")
+ax3.set(xlim=[0,10])
+ax3.legend(loc='upper right', frameon=False, fontsize=5)
+ax3.set(xlabel="Log of Throughput", ylabel="Doc Numbers")
 
 nai_j_y_data = []
-nai_j_y_data.append(nai_j_throught_put[1])
-nai_j_y_data.append(nai_j_throught_put[3])
-nai_j_y_data.append(nai_j_throught_put[5])
+nai_j_y_data.append(nai_j_throught_put_log[1])
+nai_j_y_data.append(nai_j_throught_put_log[3])
+nai_j_y_data.append(nai_j_throught_put_log[5])
 sgx_j_y_data = []
-sgx_j_y_data.append(sgx_j_throught_put[1])
-sgx_j_y_data.append(sgx_j_throught_put[3])
-sgx_j_y_data.append(sgx_j_throught_put[5])
-# sgx_pure_j_y_data = [900, 870, 700]
-cdb_j_y_data = [120,100,80]
+sgx_j_y_data.append(sgx_j_throught_put_log[1])
+sgx_j_y_data.append(sgx_j_throught_put_log[3])
+sgx_j_y_data.append(sgx_j_throught_put_log[5])
+cdb_j_y_data = []
+cdb_j_y_data.append(cdb_j_throught_put_log[1])
+cdb_j_y_data.append(cdb_j_throught_put_log[3])
+cdb_j_y_data.append(cdb_j_throught_put_log[5])
+
 ax4.set(title='(d). nested join')
 ax4.grid(linestyle='--', zorder=1)
 ax4.barh(y=range(len(op_j_x_data)), width=nai_j_y_data, label='Baseline', zorder=2, color='slategrey', alpha=1, height=bar_width)
-# ax4.barh(y=np.arange(len(op_j_x_data))+bar_width, width=sgx_pure_j_y_data, label='QShield', zorder=2, color='forestgreen', alpha=1, height=bar_width)
 ax4.barh(y=np.arange(len(op_j_x_data))+ 1*bar_width, width=sgx_j_y_data, label='QShield', zorder=2, color='chocolate', alpha=1, height=bar_width)
 ax4.barh(y=np.arange(len(op_j_x_data))+ 2*bar_width, width=cdb_j_y_data, label='CryptDB', zorder=2, color='forestgreen', alpha=1, height=bar_width)
 ax4.set_yticks(np.arange(len(op_j_x_data))+bar_width)
 ax4.set_yticklabels(op_j_x_data)
-ax4.set(xlim=[0,1500])
-ax4.legend(loc='upper right', frameon=False, fontsize=6)
-ax4.set(xlabel="Throughput (joiners/sec)", ylabel="Doc Numbers")
+ax4.set(xlim=[-1,10])
+ax4.legend(loc='upper right', frameon=False, fontsize=5)
+ax4.set(xlabel="Log of Throughput", ylabel="Doc Numbers")
 
 
 # style_list = ["g+-", "b*-", "c.-", "yo-", "k+-", "k*-", "k.-", "ko-"]
@@ -214,13 +258,19 @@ ax4.set(xlabel="Throughput (joiners/sec)", ylabel="Doc Numbers")
 x_data = ['10','1K','10K','100K','200K', '400K', '600K', '800K', '1M']
 ax5.set(title='(e). decryption')
 ax5.grid(linestyle='--', zorder=1)
+# ax5.bar(x=np.arange(len(x_data))+ 0.5*bar_width, height=e_exe_time_h_ms, label='E-scheme', color='chocolate', alpha=1, zorder=2)
+# ax5.bar(x=np.arange(len(x_data))+ 0.5*bar_width, height=sgx_exe_time_h_ms, label='SGX', color='forestgreen', alpha=1, zorder=3)
+# for x, y in enumerate(e_exe_time_h_ms):
+#     ax5.text(x+ 0.5*bar_width, y, '%s' % y, ha='center', va='bottom')
+# for x, y in enumerate(sgx_exe_time_h_ms):
+#     ax5.text(x+ 0.5*bar_width, y, '%s' % y, ha='center', va='bottom')
 ax5.bar(x=x_data, height=e_exe_time_h_ms, label='E-scheme', color='chocolate', alpha=1, zorder=2)
 ax5.bar(x=x_data, height=sgx_exe_time_h_ms, label='SGX', color='forestgreen', alpha=1, zorder=3)
 for x, y in enumerate(e_exe_time_h_ms):
     ax5.text(x, y, '%s' % y, ha='center', va='bottom')
 for x, y in enumerate(sgx_exe_time_h_ms):
     ax5.text(x, y, '%s' % y, ha='center', va='bottom')
-ax5.set(xlabel="Data size (KBytes)", ylabel="avg exe time (ms)")
+ax5.set(xlabel="Data size (Bytes)", ylabel="avg exe time (ms)")
 ax5.set(ylim=[0, 4])
 ax5.legend(loc='upper left', frameon=False, fontsize=8)
 
