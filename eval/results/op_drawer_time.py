a9d0c51ed8a04b963240817b6099f124a2e5a84b
YX-SGX <cyx.xjtu@gmail.com>
2019-12-26 11:56:14 +0800

    op_drawer_time update

--- eval/results/op_drawer_time.py
+++ eval/results/op_drawer_time.py
@@ -172,11 +172,11 @@ ax1.barh(y=range(len(op_x_data)), width=nai_p_y_data, label='Baseline', zorder=2
 ax1.barh(y=np.arange(len(op_x_data))+ 1*bar_width, width=sgx_p_y_data, label='QShield', zorder=2, color='chocolate', edgecolor='dimgray', linewidth=0.5, alpha=1, height=bar_width)
 ax1.barh(y=np.arange(len(op_x_data))+ 2*bar_width, width=cdb_p_y_data, label='CryptDB', zorder=2, color='forestgreen', edgecolor='dimgray', linewidth=0.5, alpha=1, height=bar_width)
 for y, x in enumerate(nai_p_y_data):
-    ax1.text(x+0.4, y-0.16, '%s ; %s' % (float('%.2f' % x), float('%.2f' % nai_p_exe_time_ms[y])), ha='center', va='bottom', fontdict=font4)
+    ax1.text(x+0.4, y-0.17, '%s ; %s' % (float('%.2f' % x), float('%.2f' % nai_p_exe_time_ms[y])), ha='center', va='bottom', fontdict=font4)
 for y, x in enumerate(sgx_p_y_data):
-    ax1.text(x+0.4, y+0.04, '%s ; %s' % (float('%.2f' % x), float('%.2f' % sgx_p_exe_time_ms[y])), ha='center', va='bottom', fontdict=font4)
+    ax1.text(x+0.4, y+0.03, '%s ; %s' % (float('%.2f' % x), float('%.2f' % sgx_p_exe_time_ms[y])), ha='center', va='bottom', fontdict=font4)
 for y, x in enumerate(cdb_p_y_data):
-    ax1.text(x+0.4, y+0.25, '%s ; %s' % (float('%.2f' % x), float('%.2f' % cdb_p_exe_time_ms[y])), ha='center', va='bottom', fontdict=font4)
+    ax1.text(x+0.4, y+0.24, '%s ; %s' % (float('%.2f' % x), float('%.2f' % cdb_p_exe_time_ms[y])), ha='center', va='bottom', fontdict=font4)
 ax1.set_yticks(np.arange(len(op_x_data))+bar_width)
 ax1.set_yticklabels(op_x_data)
 x_minor_locator = MultipleLocator(0.2)
@@ -210,11 +210,11 @@ ax2.barh(y=range(len(op_x_data)), width=nai_s_y_data, label='Baseline', zorder=2
 ax2.barh(y=np.arange(len(op_x_data))+ 1*bar_width, width=sgx_s_y_data, label='QShield', zorder=2, color='chocolate', edgecolor='dimgray', linewidth=0.5, alpha=1, height=bar_width)
 ax2.barh(y=np.arange(len(op_x_data))+ 2*bar_width, width=cdb_s_y_data, label='CryptDB', zorder=2, color='forestgreen', edgecolor='dimgray', linewidth=0.5, alpha=1, height=bar_width)
 for y, x in enumerate(nai_s_y_data):
-    ax2.text(x+0.4, y-0.16, '%s ; %s' % (float('%.2f' % x), float('%.2f' % nai_s_exe_time_ms[y])), ha='center', va='bottom', fontdict=font4)
+    ax2.text(x+0.4, y-0.17, '%s ; %s' % (float('%.2f' % x), float('%.2f' % nai_s_exe_time_ms[y])), ha='center', va='bottom', fontdict=font4)
 for y, x in enumerate(sgx_s_y_data):
-    ax2.text(x+0.4, y+0.04, '%s ; %s' % (float('%.2f' % x), float('%.2f' % sgx_s_exe_time_ms[y])), ha='center', va='bottom', fontdict=font4)
+    ax2.text(x+0.4, y+0.03, '%s ; %s' % (float('%.2f' % x), float('%.2f' % sgx_s_exe_time_ms[y])), ha='center', va='bottom', fontdict=font4)
 for y, x in enumerate(cdb_s_y_data):
-    ax2.text(x+0.4, y+0.25, '%s ; %s' % (float('%.2f' % x), float('%.2f' % cdb_s_exe_time_ms[y])), ha='center', va='bottom', fontdict=font4)
+    ax2.text(x+0.4, y+0.24, '%s ; %s' % (float('%.2f' % x), float('%.2f' % cdb_s_exe_time_ms[y])), ha='center', va='bottom', fontdict=font4)
 ax2.set_yticks(np.arange(len(op_x_data))+bar_width)
 ax2.set_yticklabels(op_x_data)
 x_minor_locator = MultipleLocator(0.2)
@@ -248,11 +248,11 @@ ax3.barh(y=range(len(op_x_data)), width=nai_a_y_data, label='Baseline', zorder=2
 ax3.barh(y=np.arange(len(op_x_data))+ 1*bar_width, width=sgx_a_y_data, label='QShield', zorder=2, color='chocolate', edgecolor='dimgray', linewidth=0.5, alpha=1, height=bar_width)
 ax3.barh(y=np.arange(len(op_x_data))+ 2*bar_width, width=cdb_a_y_data, label='CryptDB', zorder=2, color='forestgreen', edgecolor='dimgray', linewidth=0.5, alpha=1, height=bar_width)
 for y, x in enumerate(nai_a_y_data):
-    ax3.text(x+0.4, y-0.16, '%s ; %s' % (float('%.2f' % x), float('%.2f' % nai_a_exe_time_ms[y])), ha='center', va='bottom', fontdict=font4)
+    ax3.text(x+0.4, y-0.17, '%s ; %s' % (float('%.2f' % x), float('%.2f' % nai_a_exe_time_ms[y])), ha='center', va='bottom', fontdict=font4)
 for y, x in enumerate(sgx_a_y_data):
-    ax3.text(x+0.4, y+0.04, '%s ; %s' % (float('%.2f' % x), float('%.2f' % sgx_a_exe_time_ms[y])), ha='center', va='bottom', fontdict=font4)
+    ax3.text(x+0.4, y+0.03, '%s ; %s' % (float('%.2f' % x), float('%.2f' % sgx_a_exe_time_ms[y])), ha='center', va='bottom', fontdict=font4)
 for y, x in enumerate(cdb_a_y_data):
-    ax3.text(x+0.4, y+0.25, '%s ; %s' % (float('%.2f' % x), float('%.2f' % cdb_a_exe_time_ms[y])), ha='center', va='bottom', fontdict=font4)
+    ax3.text(x+0.4, y+0.24, '%s ; %s' % (float('%.2f' % x), float('%.2f' % cdb_a_exe_time_ms[y])), ha='center', va='bottom', fontdict=font4)
 ax3.set_yticks(np.arange(len(op_x_data))+bar_width)
 ax3.set_yticklabels(op_x_data)
 x_minor_locator = MultipleLocator(0.2)
@@ -286,11 +286,11 @@ ax4.barh(y=range(len(op_j_x_data)), width=nai_j_y_data, label='Baseline', zorder
 ax4.barh(y=np.arange(len(op_j_x_data))+ 1*bar_width, width=sgx_j_y_data, label='QShield', zorder=2, color='chocolate', edgecolor='dimgray', linewidth=0.5, alpha=1, height=bar_width)
 ax4.barh(y=np.arange(len(op_j_x_data))+ 2*bar_width, width=cdb_j_y_data, label='CryptDB', zorder=2, color='forestgreen', edgecolor='dimgray', linewidth=0.5, alpha=1, height=bar_width)
 for y, x in enumerate(nai_j_y_data):
-    ax4.text(x+0.4, y-0.16, '%s ; %s' % (float('%.2f' % x), float('%.2f' % nai_j_exe_time_ms[y])), ha='center', va='bottom', fontdict=font4)
+    ax4.text(x+0.4, y-0.17, '%s ; %s' % (float('%.2f' % x), float('%.2f' % nai_j_exe_time_ms[y])), ha='center', va='bottom', fontdict=font4)
 for y, x in enumerate(sgx_j_y_data):
-    ax4.text(x+0.4, y+0.04, '%s ; %s' % (float('%.2f' % x), float('%.2f' % sgx_j_exe_time_ms[y])), ha='center', va='bottom', fontdict=font4)
+    ax4.text(x+0.4, y+0.03, '%s ; %s' % (float('%.2f' % x), float('%.2f' % sgx_j_exe_time_ms[y])), ha='center', va='bottom', fontdict=font4)
 for y, x in enumerate(cdb_j_y_data):
-    ax4.text(x+0.4, y+0.25, '%s ; %s' % (float('%.2f' % x), float('%.2f' % cdb_j_exe_time_ms[y])), ha='center', va='bottom', fontdict=font4)
+    ax4.text(x+0.4, y+0.24, '%s ; %s' % (float('%.2f' % x), float('%.2f' % cdb_j_exe_time_ms[y])), ha='center', va='bottom', fontdict=font4)
 ax4.set_yticks(np.arange(len(op_j_x_data))+bar_width)
 ax4.set_yticklabels(op_j_x_data)
 x_minor_locator = MultipleLocator(0.2)
@@ -308,8 +308,8 @@ ax4.set_ylabel('No. of Documents', font3)
 x_data = ['10', '100', '1K','10K','100K','200K', '400K', '600K', '800K', '1M']
 ax5.set_title('(e). decryption', font2)
 ax5.grid(linestyle='--', axis='y', zorder=1)
-ax5.bar(x=x_data, height=e_exe_time_h_ms_log, label='E-Scheme', color='chocolate', edgecolor='dimgray', linewidth=0.5, alpha=1, zorder=2)
-ax5.bar(x=x_data, height=sgx_exe_time_h_ms_log, label='SGX', color='forestgreen', edgecolor='dimgray', linewidth=0.5, alpha=1, zorder=3)
+ax5.bar(x=x_data, height=e_exe_time_h_ms_log, label='QShield', color='chocolate', edgecolor='dimgray', linewidth=0.5, alpha=1, zorder=2)
+ax5.bar(x=x_data, height=sgx_exe_time_h_ms_log, label='Baseline', color='forestgreen', edgecolor='dimgray', linewidth=0.5, alpha=1, zorder=3)
 for x, y in enumerate(e_exe_time_h_ms_log):
     ax5.text(x, y + 0.1, '%s ; %s' % (float('%.2f' % y), float('%.2f' % e_exe_time_h_ms[x])), ha='center', va='bottom', fontdict=font4)
 for x, y in enumerate(sgx_exe_time_h_ms_log):
@@ -320,10 +320,10 @@ y_minor_locator = MultipleLocator(0.2)
 y_major_locator = MultipleLocator(1)
 ax5.yaxis.set_minor_locator(y_minor_locator)
 ax5.yaxis.set_major_locator(y_major_locator)
-ax5.set(ylim=[-2, 2])
+ax5.set(ylim=[-2, 1])
 labels = ax5.get_xticklabels() + ax5.get_yticklabels()
 [label.set_fontname('Times New Roman') for label in labels]
-ax5.legend(loc='upper left', frameon=True, prop=font1)
+ax5.legend(loc='best', frameon=True, prop=font1)
 
 plt.tight_layout()
 plt.show()
